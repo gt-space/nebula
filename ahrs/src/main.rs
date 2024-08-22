e18721fb74f176e8d8d2d3ee4ba0c538feaef4cd
Tymur Tkachenko <ttkachenko3@gatech.edu>
2024-08-22 14:11:37 +0800

    imu test

--- ahrs/src/main.rs
+++ ahrs/src/main.rs
@@ -5,106 +5,71 @@ use std::time::Duration;
 pub mod gpio;
 
 fn main() {
-  // let mut spidev = Spidev::open("/dev/spidev0.0").unwrap();
-  // let spidev_options = SpidevOptions::new()
-  // 	.lsb_first(false)
-  // 	.bits_per_word(8)
-  // 	.max_speed_hz(2_000_000)
-  // 	.mode(SpiModeFlags::SPI_MODE_3)
-  // 	.build();
-  // spidev.configure(&spidev_options)
-  // 	.expect("failed to configure SPI for the IMU");
+  const IMU_CS: &str = "11";
+  const IMU_NRST: &str = "89";
+  const IMU_DR: &str = "81";
 
-  let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
-  let options = SpidevOptions::new()
-    .bits_per_word(16)
-    .max_speed_hz(2_000_000)
-    .lsb_first(false)
-    .mode(SpiModeFlags::SPI_MODE_3)
-    .build();
-  spi.configure(&options).unwrap();
+  const MAG_CS: &str = "46";
+  const MAG_DR: &str = "65";
+  const MAG_INT: &str = "61";
 
-  // gpio::set_output("47");
-  // gpio::set_high("47");
-  // gpio::set_output("62");
-  // gpio::set_high("62");
-  // gpio::set_output("67");
-  // gpio::set_high("67");
-  // gpio::set_output("68");
-  // gpio::set_high("68");
-  // gpio::set_output("36");
-  // gpio::set_high("36");
+  const BAR_CS: &str = "88";
 
-  gpio::set_output("68");
-  gpio::set_low("36");
+  let mut spi = Spidev::open("/dev/spidev0.0").expect("Failed to initalize SPI");
+  let mut imu = SpidevOptions::new();
+  let mut mag = SpidevOptions::new();
+  let mut bar = SpidevOptions::new();
 
-  // // 0xE880, then 0xE900
-  // let tx_buf = [0xE8, 0x80];
-  // let mut rx_buf = [0; 2];
-  // let mut transfer = SpidevTransfer::read_write(&tx_buf, &mut rx_buf);
-  // spidev.transfer(&mut transfer).expect("Could not transfer over SPI");
-  // println!("{:?}", rx_buf);
+  gpio::set_output(IMU_NRST);
+  gpio::set_low(IMU_NRST);
+  thread::sleep(Duration::from_micros(200));
+  gpio::set_high(IMU_NRST);
 
-  // let tx_buf = [0xE9, 0x00];
-  // let mut rx_buf = [0; 2];
-  // let mut transfer = SpidevTransfer::read_write(&tx_buf, &mut rx_buf);
-  // spidev.transfer(&mut transfer).expect("Could not transfer over SPI");
-  // println!("{:?}", rx_buf);
+  gpio::set_output(IMU_CS);
+  gpio::set_high(IMU_CS);
 
-  // thread::sleep(Duration::from_secs(5));
+  gpio::set_output(MAG_CS);
+  gpio::set_high(MAG_CS);
+
+  gpio::set_output(BAR_CS);
+  gpio::set_high(BAR_CS);
+
+  gpio::set_input(IMU_DR);
+  gpio::set_input(MAG_DR);
+  gpio::set_input(MAG_INT);
 
   loop {
-    // read_imu(&imu);
-
-    // let mut rx_buf = [0_u8; 2];
-    // imu.write(&[0x0C, 0x0E]).expect("Could not write tx buffer");
-    // imu.read(&mut rx_buf).expect("Could not read rx buffer");
-    // if rx_buf == [0_u8; 2] {
-    // 	println!("Failed to retrieve information over SPI");
-    // } else {
-    // 	println!("{:?}", rx_buf);
-    // }
-    let tx_buf: [u8; 2] = [0x0C, 0x00];
-    let mut rx_buf = [0; 2];
-    // spidev.write(&tx_buf).expect("Failed writing to SPI");
-    // spidev.read(&mut rx_buf).expect("Failed reading from SPI");
-    let mut transfer = SpidevTransfer::read_write(&tx_buf, &mut rx_buf);
-    spi
-      .transfer(&mut transfer)
-      .expect("Could not transfer over SPI");
-    println!("{:?}", rx_buf);
+    gpio::set_low(IMU_CS);
+    configure_imu(&mut spi, &mut imu);
+    read_imu(&mut spi);
+    gpio::set_high(IMU_CS);
 
     thread::sleep(Duration::from_micros(100));
   }
 }
 
-fn _read_imu(imu: &Spidev) {
-  // let mut rx_buf = [0_u8; 4];
-  // imu.write(&[0x01, 0x02, 0x03])?;
-  // imu.read(&mut rx_buf)?;
-  // println!("{:?}", rx_buf);
+fn read_imu(spi: &mut Spidev) {
+  read_spi2([0x1C, 0x00], spi, String::from("TEMP"));
+  read_spi2([0x1E, 0x00], spi, String::from("TIME"));
 
-  _read_spi2([0x1C, 0x00], imu, String::from("TEMP"));
-  // read_spi2([0x1E, 0x00], &imu, String::from("TIME"));
+  read_spi4([0x04, 0x00, 0x06, 0x00], spi, String::from("X_GYRO"));
+  read_spi4([0x08, 0x00, 0x0A, 0x00], spi, String::from("Y_GYRO"));
+  read_spi4([0x0C, 0x00, 0x0E, 0x00], spi, String::from("Z_GYRO"));
 
-  // read_spi4([0x04, 0x00, 0x06, 0x00], &imu, String::from("X_GYRO"));
-  // read_spi4([0x08, 0x00, 0x0A, 0x00], &imu, String::from("Y_GYRO"));
-  // read_spi4([0x0C, 0x00, 0x0E, 0x00], &imu, String::from("Z_GYRO"));
+  read_spi4([0x10, 0x00, 0x12, 0x00], spi, String::from("X_ACCL"));
+  read_spi4([0x14, 0x00, 0x16, 0x00], spi, String::from("Y_ACCL"));
+  read_spi4([0x18, 0x00, 0x1A, 0x00], spi, String::from("Z_ACCL"));
 
-  // read_spi4([0x10, 0x00, 0x12, 0x00], &imu, String::from("X_ACCL"));
-  // read_spi4([0x14, 0x00, 0x16, 0x00], &imu, String::from("Y_ACCL"));
-  // read_spi4([0x18, 0x00, 0x1A, 0x00], &imu, String::from("Z_ACCL"));
+  read_spi4([0x24, 0x00, 0x26, 0x00], spi, String::from("X_DELTANG"));
+  read_spi4([0x28, 0x00, 0x2A, 0x00], spi, String::from("Y_DELTANG"));
+  read_spi4([0x2C, 0x00, 0x2E, 0x00], spi, String::from("Z_DELTANG"));
 
-  // read_spi4([0x24, 0x00, 0x26, 0x00], &imu, String::from("X_DELTANG"));
-  // read_spi4([0x28, 0x00, 0x2A, 0x00], &imu, String::from("Y_DELTANG"));
-  // read_spi4([0x2C, 0x00, 0x2E, 0x00], &imu, String::from("Z_DELTANG"));
-
-  // read_spi4([0x30, 0x00, 0x32, 0x00], &imu, String::from("X_DELTVEL"));
-  // read_spi4([0x34, 0x00, 0x36, 0x00], &imu, String::from("Y_DELTVEL"));
-  // read_spi4([0x38, 0x00, 0x3A, 0x00], &imu, String::from("Z_DELTVEL"));
+  read_spi4([0x30, 0x00, 0x32, 0x00], spi, String::from("X_DELTVEL"));
+  read_spi4([0x34, 0x00, 0x36, 0x00], spi, String::from("Y_DELTVEL"));
+  read_spi4([0x38, 0x00, 0x3A, 0x00], spi, String::from("Z_DELTVEL"));
 }
 
-fn _read_spi4(tx_buf: [u8; 4], spi: &Spidev, s: String) {
+fn read_spi4(tx_buf: [u8; 4], spi: &Spidev, s: String) {
   let mut rx_buf = [0; 4];
   let mut transfer = SpidevTransfer::read_write(&tx_buf, &mut rx_buf);
   let result = spi.transfer(&mut transfer);
@@ -116,7 +81,7 @@ fn _read_spi4(tx_buf: [u8; 4], spi: &Spidev, s: String) {
   }
 }
 
-fn _read_spi2(tx_buf: [u8; 2], spi: &Spidev, s: String) {
+fn read_spi2(tx_buf: [u8; 2], spi: &Spidev, s: String) {
   let mut rx_buf = [0; 2];
   let mut transfer = SpidevTransfer::read_write(&tx_buf, &mut rx_buf);
   let result = spi.transfer(&mut transfer);
@@ -126,4 +91,31 @@ fn _read_spi2(tx_buf: [u8; 2], spi: &Spidev, s: String) {
     }
     Err(err) => println!("{:?}", err),
   }
+}
+
+fn configure_imu(spi: &mut Spidev, imu: &mut SpidevOptions) {
+  imu.bits_per_word(8);
+  imu.max_speed_hz(2_000_000);
+  imu.lsb_first(false);
+  imu.mode(SpiModeFlags::SPI_MODE_0);
+  imu.build();
+  spi.configure(imu).expect("Failed to configure SPI for the IMU");
+}
+
+fn configure_mag(spi: &mut Spidev, mag: &mut SpidevOptions) {
+  mag.bits_per_word(8);
+  mag.max_speed_hz(10_000_000);
+  mag.lsb_first(false);
+  mag.mode(SpiModeFlags::SPI_MODE_3);
+  mag.build();
+  spi.configure(mag).expect("Failed to configure SPI for the Magnetometer");
+}
+
+fn configure_bar(spi: &mut Spidev, bar: &mut SpidevOptions) {
+  bar.bits_per_word(8);
+  bar.max_speed_hz(20_000_000);
+  bar.lsb_first(false);
+  bar.mode(SpiModeFlags::SPI_MODE_0);
+  bar.build();
+  spi.configure(bar).expect("Failed to configure SPI for the Barometer");
 }
