17d8bb0c33e64fef868afd7dcb49cbf9de85259b
georgegoldenbank <guccirhombus1@gmail.com>
2024-09-24 12:52:33 -0400

    added some documentation about the virtual memory operations

--- common/src/comm/gpio.rs
+++ common/src/comm/gpio.rs
@@ -55,6 +55,15 @@ impl Gpio {
       panic!("Cannot open memory device");
     }
 
+    /*
+    mmap creates a block of virtual memory that is mapped to the hw registers.
+    Here we cannot interact with the actual register locations directly
+    because there is an OS in the way so we use the virtual memory locations.
+    When we pass the virtual memory location values, the OS will know how
+    to map those operations to the actual register locations because of the
+    relationship that was defined through the mmap call. The return value of
+    mmap is a pointer to the start address of the virtual memory block.
+     */
     let base = unsafe {
       libc::mmap(
         std::ptr::null_mut(),
@@ -64,24 +73,29 @@ impl Gpio {
         fd,
         GPIO_BASE_REGISTERS[controller_indexindex],
       )
-    };
+    } as *const u8;
 
     if base.is_null() {
       panic!("Cannot map GPIO");
     }
 
-    if base != GPIO_BASE_REGISTERS[controller_index] as *mut c_void {
-      panic!("Invalid start address for GPIO DMA operations");
-    }
+    // if base != GPIO_BASE_REGISTERS[controller_index] as *mut c_void {
+    //   panic!("Invalid start address for GPIO DMA operations");
+    // }
     // } else if base != GPIO_BASE_REGISTERS[index] as *mut c_void {
     //     panic!("Cannot acquire GPIO at {index}. Did you call Gpio::open
     // twice?"); }
 
+    /*
+    All of the following registers are 32 bits wide, hence the u32 cast.
+    These values are still pointers to virtual memory addresses
+     */
     let oe = unsafe { base.offset(GPIO_OE_REGISTER) as *mut u32 };
 
     let dataout = unsafe { base.offset(GPIO_DATAOUT_REGISTER) as *mut u32 };
 
-    let datain = unsafe { base.offset(GPIO_DATAIN_REGISTER) as *mut u32 };
+    // changed cast from *mut u32 to *const u32 because it is only being read
+    let datain = unsafe { base.offset(GPIO_DATAIN_REGISTER) as *const u32 };
 
     Gpio {
       fd,
@@ -102,18 +116,18 @@ impl Gpio {
 
 impl Pin {
   pub fn mode(&mut self, mode: PinMode) {
-    let mut bits = unsafe { std::ptr::read_volatile(self.gpio.oe) };
+    let mut bits = unsafe { std::ptr::read_volatile(*(self.gpio.oe)) };
 
     bits = match mode {
       PinMode::Output => bits & !(1 << self.index),
       PinMode::Input => bits | (1 << self.index),
     };
 
-    unsafe { std::ptr::write_volatile(self.gpio.oe, bits) };
+    unsafe { std::ptr::write_volatile(*(self.gpio.oe), bits) };
   }
 
   pub fn digital_write(&mut self, value: PinValue) {
-    let mut bits = unsafe { std::ptr::read_volatile(self.gpio.dataout) };
+    let mut bits = unsafe { std::ptr::read_volatile(*(self.gpio.dataout)) };
 
     bits = match value {
       PinValue::Low => bits & !(1 << self.index),
@@ -124,7 +138,7 @@ impl Pin {
   }
 
   pub fn digital_read(&self) -> PinValue {
-    let bits = unsafe { std::ptr::read_volatile(self.gpio.datain) };
+    let bits = unsafe { std::ptr::read_volatile(*(self.gpio.datain)) };
 
     if bits & (1 << self.index) != 0 {
       PinValue::High
