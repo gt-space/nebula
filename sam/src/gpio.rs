c24dae120a5f78e105ba37b9b918cdb53b23129b
georgegoldenbank <guccirhombus1@gmail.com>
2024-10-02 01:49:29 -0400

    Changed everything to use references to AtomicdU32 and a bit iffy on the base pointer casting

--- sam/src/gpio.rs
+++ sam/src/gpio.rs
@@ -14,11 +14,14 @@
 // Table 12 and 13 were used to determine the P[8/9]_pin_number on expansion
 // header -> gpio controller value in chip
 
+use common::comm::PinValue;
 use libc::{c_int, c_void, off_t, size_t};
 use std::{
   ffi::CString,
   sync::{atomic::{AtomicU32, Ordering}, Arc, Mutex},
 };
+
+use vol
 
 const GPIO_BASE_REGISTERS: [off_t; 4] =
   [0x44E0_7000, 0x4804_C000, 0x481A_C000, 0x481A_E000];
@@ -54,21 +57,21 @@ pub enum BitOrder {
 //   datain: *const u32,
 // }
 
-pub struct Gpio {
+pub struct Gpio<'a> {
   fd: c_int,
-  base: AtomicPtr<c_void>,
-  oe: AtomicU32,
-  dataout: AtomicU32,
-  datain: AtomicU32
+  base: AtomicPtr<u8>,
+  oe: &'a AtomicU32,
+  dataout: &'a AtomicU32,
+  datain: &'a AtomicU32
 }
 
 // below impl's not needed because we are moving to single threaded application
 // unsafe impl Send for Gpio {}
 // unsafe impl Sync for Gpio {}
 
-pub struct Pin {
+pub struct Pin<'a> {
   // gpio: Arc<Gpio>,
-  gpio: Rc<Gpio>,
+  gpio: Rc<Gpio<'a>>,
   index: usize,
 }
 
@@ -113,7 +116,7 @@ impl Gpio {
         fd,
         GPIO_BASE_REGISTERS[index],
       )
-    };
+    } as *mut u8; // when doing offset this increments by bytes
 
     if base.is_null() {
       panic!("Cannot map GPIO");
@@ -122,16 +125,29 @@ impl Gpio {
     //     panic!("Cannot acquire GPIO at {index}. Did you call Gpio::open
     // twice?"); }
 
-    let oe = Mutex::new(unsafe { base.offset(GPIO_OE_REGISTER) as *mut u32 });
+    // Initialize the AtomicPtr with the memory-mapped base address
+    let base = AtomicPtr::new(base_ptr);
 
-    let dataout =
-      Mutex::new(unsafe { base.offset(GPIO_DATAOUT_REGISTER) as *mut u32 });
+    // Load the base pointer value to calculate register addresses
+    let base_loaded = base.load(Ordering::SeqCst);
 
-    let datain = unsafe { base.offset(GPIO_DATAIN_REGISTER) as *mut u32 };
+    // let oe = Mutex::new(unsafe { base.offset(GPIO_OE_REGISTER) as *mut u32 });
+    let oe_address = unsafe { base_loaded.ofset(GPIO_OE_REGISTER) as *mut AtomicU32 };
+    let oe = unsafe { &*oe_address };
+
+    // let dataout =
+    //   Mutex::new(unsafe { base.offset(GPIO_DATAOUT_REGISTER) as *mut u32 });
+    
+    let dataout_address = unsafe { base_loaded.ofset(GPIO_DATAOUT_REGISTER) as *mut AtomicU32 };
+    let dataout = unsafe{ &*dataout_address} ;
+
+    // let datain = unsafe { base.offset(GPIO_DATAIN_REGISTER) as *mut u32 };
+    let datain_address = unsafe { base_loaded.offset(GPIO_DATAIN_REGISTER) as *const AtomicU32 };
+    let datain = unsafe { &*datain_address };
 
     let base = Mutex::new(base);
 
-    Arc::new(Gpio {
+    Rc::new(Gpio {
       fd,
       base,
       oe,
@@ -149,35 +165,65 @@ impl Gpio {
 }
 
 impl Pin {
+  // pub fn mode(&self, mode: PinMode) {
+  //   let oe = self.gpio.oe.lock().unwrap();
+  //   let mut bits = unsafe { std::ptr::read_volatile(*oe) };
+
+  //   bits = match mode {
+  //     PinMode::Output => bits & !(1 << self.index),
+  //     PinMode::Input => bits | (1 << self.index),
+  //   };
+
+  //   unsafe { std::ptr::write_volatile(*oe, bits) };
+  // }
+
   pub fn mode(&self, mode: PinMode) {
-    let oe = self.gpio.oe.lock().unwrap();
-    let mut bits = unsafe { std::ptr::read_volatile(*oe) };
-
-    bits = match mode {
-      PinMode::Output => bits & !(1 << self.index),
-      PinMode::Input => bits | (1 << self.index),
-    };
-
-    unsafe { std::ptr::write_volatile(*oe, bits) };
+    let bits = 1 << self.index;
+    match mode {
+      PinMode::Input => self.gpio.oe.fetch_or(bits, Ordering::SeqCst),
+      PinMode::Output => self.gpio.oe.fetch_and(!bit, Ordering::SeqCst)
+    }
   }
 
+  // pub fn digital_write(&self, value: PinValue) {
+  //   let dataout = self.gpio.dataout.lock().unwrap();
+  //   let mut bits = unsafe { std::ptr::read_volatile(*dataout) };
+
+  //   bits = match value {
+  //     PinValue::Low => bits & !(1 << self.index),
+  //     PinValue::High => bits | (1 << self.index),
+  //   };
+
+  //   unsafe { std::ptr::write_volatile(*dataout, bits) };
+  // }
   pub fn digital_write(&self, value: PinValue) {
-    let dataout = self.gpio.dataout.lock().unwrap();
-    let mut bits = unsafe { std::ptr::read_volatile(*dataout) };
+    let bits = 1 << self.index;
+    match value {
+      PinValue::Low => {
+        self.gpio.dataout.fetch_and(!bits, Ordering::SeqCst);
+      },
 
-    bits = match value {
-      PinValue::Low => bits & !(1 << self.index),
-      PinValue::High => bits | (1 << self.index),
-    };
-
-    unsafe { std::ptr::write_volatile(*dataout, bits) };
+      PinValue::High => {
+        self.gpio.dataout.fetch_or(bits, Ordering::SeqCst);
+      }
+    }
   }
+
+  // pub fn digital_read(&self) -> PinValue {
+  //   let datain = self.gpio.datain;
+  //   let bits = unsafe { std::ptr::read_volatile(datain) };
+
+  //   if bits & (1 << self.index) != 0 {
+  //     PinValue::High
+  //   } else {
+  //     PinValue::Low
+  //   }
+  // }
 
   pub fn digital_read(&self) -> PinValue {
-    let datain = self.gpio.datain;
-    let bits = unsafe { std::ptr::read_volatile(datain) };
+    let datain = self.gpio.datain.load(Ordering::SeqCst);
 
-    if bits & (1 << self.index) != 0 {
+    if datain & (1 << self.index) != 0 {
       PinValue::High
     } else {
       PinValue::Low
