caec0b04bbe8f7891f947da748b79cb3889dda19
panya <pgbhinder@gmail.com>
2024-10-17 14:49:00 -0400

    AVI 208: add blank BMS page

---
+++ gui/src/avi/bms/bms.tsx
@@ -0,0 +1,35 @@
+import { For, createEffect, createSignal } from "solid-js";
+import Footer from "../../general-components/Footer";
+import { GeneralTitleBar } from "../../general-components/TitleBar";
+import { listen } from "@tauri-apps/api/event";
+import { invoke } from "@tauri-apps/api/tauri";
+import { appWindow } from "@tauri-apps/api/window";
+import { Config, Sequence, State, runSequence, serverIp, StreamState } from "../../comm";
+
+const [configurations, setConfigurations] = createSignal();
+const [activeConfig, setActiveConfig] = createSignal();
+const [activeBoards, setActiveBoards] = createSignal();
+
+listen('state', (event) => {
+  console.log(event.windowLabel);
+  setConfigurations((event.payload as State).configs);
+  setActiveConfig((event.payload as State).activeConfig);
+});
+
+invoke('initialize_state', {window: appWindow});
+
+function BMS() {
+    return <div class="window-template">
+    <div style="height: 60px">
+      <GeneralTitleBar name="BMS"/>
+    </div>
+    <div class="bms-view">
+      
+    </div>
+    <div>
+      <Footer/>
+    </div>
+</div>
+}
+
+export default BMS;
