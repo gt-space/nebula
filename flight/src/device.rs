46197c626f14329732de397b52d0652400392abc
sgarg330 <sgarg330@gatech.edu>
2024-11-07 08:01:21 -0500

    inital implementation for collecting data

--- flight/src/device.rs
+++ flight/src/device.rs
@@ -1,6 +1,8 @@
 use std::{net::{TcpListener, TcpStream, SocketAddr}, sync::LazyLock};
 use super::{Data, LISTENER_ADDRESS};
 use std::io::ErrorKind;
+use std::io::Read;
+
 
 
 static DEVICE_LISTENER: LazyLock<TcpListener> = LazyLock::new(|| {
@@ -38,5 +40,34 @@ pub(super) fn pull<'a, T>(devices: T) -> Vec<Data>
 where
  T: Iterator<Item = &'a mut TcpStream>,
 {
- panic!("device::pull: Not Implemented!");
+  const DATA_BUFFER_SIZE: usize = 1024; // ? 
+  let mut collected_data = Vec::new();
+  for stream in devices{
+    let mut buffer = [0; DATA_BUFFER_SIZE];
+    stream.set_nonblocking(true).expect("set_nonblocking call failed");
+
+    match stream.read(&mut buffer) {
+      Ok(0) => {
+          // handle connection dropped 
+          eprintln!("connection has been dropped");
+          continue;
+      }
+      Ok(n) => {
+        //how should I convert the data to Data Struct?
+        collected_data.push(buffer[..n].to_vec());
+
+      }
+      Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+          // No data; should check heartbeat timer here?
+          continue;
+      }
+      Err(e) => {
+          eprintln!("Error reading from stream: {}", e);
+      }
+  }
 }
+
+collected_data
+}
+
+
