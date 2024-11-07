6a8ab001c82c5cb8035de08bc6368c68728ac2c5
sgarg330 <sgarg330@gatech.edu>
2024-11-07 02:38:25 -0500

    added method to look for new TCP device connections

--- flight/src/device.rs
+++ flight/src/device.rs
@@ -1,20 +1,42 @@
 use std::{net::{TcpListener, TcpStream, SocketAddr}, sync::LazyLock};
 use super::{Data, LISTENER_ADDRESS};
+use std::io::ErrorKind;
+
 
 static DEVICE_LISTENER: LazyLock<TcpListener> = LazyLock::new(|| {
-  TcpListener::bind(LISTENER_ADDRESS)
-    .expect("Could not bind to device socket address.")
+ TcpListener::bind(LISTENER_ADDRESS)
+   .expect("Could not bind to device socket address.")
 });
+
 
 /// Returns all newfound device streams
 pub(super) fn listen() -> Vec<(SocketAddr, TcpStream)> {
-  panic!("device::listen: Not Implemented!");
-}
+
+
+    let mut connections = Vec::new();
+     // Set the listener to non-blocking mode to avoid blocking the main thread
+    DEVICE_LISTENER.set_nonblocking(true).expect("Failed to set non-blocking mode.");
+     loop {
+        match DEVICE_LISTENER.accept() {
+            Ok((stream, addr)) => {
+                // If a connection is accepted, add it to the list
+                connections.push((addr, stream));
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                // Not ready yet so we will check in next stream
+                break;
+            }
+            Err(e) => panic!("encountered IO error: {e}"),         
+        }
+    }
+    connections
+ }
+
 
 /// Collects all incoming data from device streams
 pub(super) fn pull<'a, T>(devices: T) -> Vec<Data>
-where 
-  T: Iterator<Item = &'a mut TcpStream>,
+where
+ T: Iterator<Item = &'a mut TcpStream>,
 {
-  panic!("device::pull: Not Implemented!");
+ panic!("device::pull: Not Implemented!");
 }
