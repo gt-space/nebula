fa8e02d33d2527b908a799ee73260a1775c3d1cd
georgegoldenbank <guccirhombus1@gmail.com>
2024-11-06 23:14:50 -0500

    deleted random comments

--- darcy_bms/src/main.rs
+++ darcy_bms/src/main.rs
@@ -24,25 +24,3 @@ fn main() {
     state = state.next();
   }
 }
-
-  // // begin FC communication
-  // let (data_socket, command_socket, fc_address) = establish_flight_computer_connection();
-  
-  // let mut then = Instant::now();
-  // loop {
-  //   println!("Checking for commands...");
-
-  //   // check if commands were sent from the FC. if so, execute them
-  //   check_and_execute(&gpio_controllers, &command_socket);
-  //   println!("Checking heartbeat...");
-
-  //   // check if the FC is sending a heartbeat. if so, reset the timer
-  //   then = check_heartbeat(&data_socket, then, &gpio_controllers);
-    
-  //   let datapoints = poll_adcs(&mut adcs);
-
-  //   println!("Sending data...");
-
-  //   // send the adc data to the FC
-  //   send_data(&data_socket, &fc_address, datapoints);
-  // }
