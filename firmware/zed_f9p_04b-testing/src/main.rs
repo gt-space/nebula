3c7360cbb3185c540e201439ba678b047b894f01
vink246 <2vineetk@gmail.com>
2025-01-02 19:32:30 -0500

    Fix compile issues and add testing script

---
+++ firmware/zed_f9p_04b-testing/src/main.rs
@@ -0,0 +1,61 @@
+use zed_f9p_04b::GPS;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+  let bus = "/dev/spidev0.0"; // Adjust this
+  let cs_pin = 0; // Adjust this
+
+  println!("Initializing GPS...");
+  match GPS::new(bus, cs_pin) {
+    Ok(mut gps) => {
+      println!("GPS initialized successfully!");
+
+      println!("Getting device version information...");
+      if let Err(e) = gps.mon_ver() {
+        eprintln!("Failed to get MON-VER message: {}", e);
+        return;
+      }
+
+      println!("Sending NAV-PVT poll request...");
+      if let Err(e) = gps.nav_pvt_poll_req() {
+        eprintln!("Failed to send NAV-PVT poll request: {}", e);
+        return;
+      }
+
+      println!("Polling PVT data...");
+      loop {
+        match gps.poll_pvt() {
+          Ok(Some(pvt)) => {
+            println!("PVT data received:");
+            if let Some(pos) = pvt.position {
+              println!(
+                "Position: Latitude {:.5}, Longitude {:.5}, Altitude {:.2}m",
+                pos.lat, pos.lon, pos.alt
+              );
+            }
+            if let Some(vel) = pvt.velocity {
+              println!(
+                "Velocity: Speed {:.2} m/s, Heading {:.2}°",
+                vel.speed, vel.heading
+              );
+            }
+            if let Some(time) = pvt.time {
+              println!("Time: {}", time);
+            }
+          }
+          Ok(None) => {
+            println!("No PVT data received.");
+          }
+          Err(e) => {
+            eprintln!("Error polling PVT data: {}", e);
+          }
+        }
+        sleep(Duration::from_millis(100));
+      }
+    }
+    Err(e) => {
+      eprintln!("Error initializing GPS: {}", e);
+    }
+  }
+}
--- firmware/zed_f9p_04b/src/lib.rs
+++ firmware/zed_f9p_04b/src/lib.rs
@@ -8,44 +8,83 @@
 use chrono::{DateTime, Utc};
 use rppal::gpio::{Gpio, OutputPin}; // Using Raspberry Pi (remember to enable SPI in raspi-config)
 use spidev::{SpiModeFlags, Spidev, SpidevOptions}; // rppal also provides Raspberry Pi SPI interface if needed
-use std::{thread, time::Duration};
+use std::{
+  fmt, io,
+  io::{Read, Write},
+  thread,
+  time::Duration,
+};
 use ublox::{
-  cfg_val::CfgVal, CfgMsgAllPortsBuilder, CfgPrtSpiBuilder, CfgValSetBuilder,
-  PacketRef, Parser, Position, Velocity,
+  CfgMsgAllPorts, CfgMsgAllPortsBuilder, GpsFix, MonVer, NavPvt, PacketRef,
+  Parser, Position, UbxPacketMeta, UbxPacketRequest, Velocity,
 };
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct PVT {
-  position: Option<Position>,
-  velocity: Option<Velocity>,
-  time: Option<DateTime<Utc>>,
+  pub position: Option<Position>,
+  pub velocity: Option<Velocity>,
+  pub time: Option<DateTime<Utc>>,
 }
 
-#[derive(Debug)]
 pub struct GPS {
   spidev: Spidev,
   // d_sel pin commented as it is already hardwired to GND on blackbox
   //d_sel: OutputPin, // SPI is disabled by default, d_sel needs to be set to 0 to enable SPI
   cs_pin: OutputPin,
-  parser: Parser,
+  parser: Parser<Vec<u8>>,
+}
+
+// Error type for instantiating GPS
+#[derive(Debug)]
+pub enum GPSError {
+  SPI(io::Error),
+  GPIO(rppal::gpio::Error),
+  GPSMessage(io::Error),
+  Configuration(String),
+}
+
+impl fmt::Display for GPSError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      GPSError::SPI(err) => write!(f, "SPI error: {}", err),
+      GPSError::GPIO(err) => write!(f, "GPIO error: {}", err),
+      GPSError::GPSMessage(err) => write!(f, "GPS Message error {}", err),
+      GPSError::Configuration(msg) => write!(f, "Configuration error: {}", msg),
+    }
+  }
+}
+
+impl std::error::Error for GPSError {}
+
+impl From<io::Error> for GPSError {
+  fn from(err: io::Error) -> Self {
+    GPSError::SPI(err)
+  }
+}
+
+impl From<rppal::gpio::Error> for GPSError {
+  fn from(err: rppal::gpio::Error) -> Self {
+    GPSError::GPIO(err)
+  }
 }
 
 impl GPS {
   pub fn new(
     bus: &str,
     /*d_sel_pin: u8,*/ cs_pin: u8,
-  ) -> std::io::Result<Self> {
+  ) -> Result<Self, GPSError> {
     // Initialize the SPI device
-    let mut spidev = Spidev::open(bus)?;
+    let mut spidev = Spidev::open(bus).map_err(GPSError::SPI)?;
     // See Datasheet Section 5.2
-    let options = SpidevOptions::new()
+    let mut options = SpidevOptions::new();
+    options
       .bits_per_word(8)
       .max_speed_hz(1_000_000) // 1 MHz to be safe
       .mode(SpiModeFlags::SPI_MODE_0); // Mode 0
-    spidev.configure(&options)?;
+    spidev.configure(&options).map_err(GPSError::SPI)?;
 
     // Initialize the ublox parser
-    let mut parser = Parser::default();
+    let parser = Parser::default();
 
     // Configure GPIO pins
     let gpio = Gpio::new()?;
@@ -69,7 +108,7 @@ impl GPS {
   // pub fn configure_gps_constellation(&mut self) -> std::io::Result<()> {
   //   let cfg_gnss = self.build_cfg_gnss_message()?;
   //   self.select_chip()?;
-  //   self.spidev.write(&cfg_gnss)?;
+  //   self.spidev.write_all(&cfg_gnss)?;
   //   self.deselect_chip()?;
   //   self.wait_for_ack()?;
   //   println!("Configured GPS constellation only.");
@@ -111,106 +150,114 @@ impl GPS {
   // Useful to test SPI communication with the module since it is
   // configuration-independent
   // See Interface Description Section 3.14.15
-  pub fn mon_ver(&mut self) -> std::io::Result<()> {
+  pub fn mon_ver(&mut self) -> Result<(), GPSError> {
     self.select_chip()?;
-    self
-      .spidev
-      .write(&UbxPacketRequest::request_for::<MonVer>().into_packet_bytes())?;
+    self.spidev.write_all(
+      &UbxPacketRequest::request_for::<MonVer>().into_packet_bytes(),
+    )?;
     self.deselect_chip()?;
     let mut found_mon_ver = false;
     thread::sleep(Duration::from_millis(500));
-    self.read_packets(|packet| match packet {
-      PacketRef::MonVer(packet) => {
-        found_mon_ver = true;
-        println!(
-          "SW version: {} HW version: {}; Extensions: {:?}",
-          packet.software_version(),
-          packet.hardware_version(),
-          packet.extension().collect::<Vec<&str>>()
-        );
-        println!("{:?}", packet);
-      }
-      _ => {
-        println!("{:?}", packet); // some other packet
-      }
-    });
+    self
+      .read_packets(|packet| match packet {
+        PacketRef::MonVer(packet) => {
+          found_mon_ver = true;
+          println!(
+            "SW version: {} HW version: {}; Extensions: {:?}",
+            packet.software_version(),
+            packet.hardware_version(),
+            packet.extension().collect::<Vec<&str>>()
+          );
+          println!("{:?}", packet);
+        }
+        _ => {
+          println!("{:?}", packet); // some other packet
+        }
+      })
+      .map_err(GPSError::GPSMessage)?;
     if found_mon_ver {
       Ok(())
     } else {
-      Err(std::io::Error::new(
+      Err(GPSError::GPSMessage(std::io::Error::new(
         std::io::ErrorKind::NotFound,
         "No UBX-MON-VER response received from the device.",
-      ))
+      )))
     }
   }
 
   // NavPvt Poll request message (I/O port id 4 is SPI)
   // Call this first to start receiving NavPvt messages
   // See Interface Description Section 3.10.10
-  pub fn nav_pvt_poll_req(&mut self) -> std::io::Result<()> {
+  pub fn nav_pvt_poll_req(&mut self) -> Result<(), GPSError> {
     self.select_chip()?;
-    self.spidev.write(
+    self.spidev.write_all(
       &CfgMsgAllPortsBuilder::set_rate_for::<NavPvt>([0, 0, 0, 0, 1, 0])
         .into_packet_bytes(),
     )?;
     self.deselect_chip()?;
+    self.wait_for_ack::<CfgMsgAllPorts>()?;
     Ok(())
   }
 
   // Polls the GPS module for a PVT (Position, Velocity, Time) message
   // See Interface Description Section 3.15.13
-  pub fn poll_pvt(&mut self) -> Option<PVT> {
+  pub fn poll_pvt(&mut self) -> Result<Option<PVT>, GPSError> {
     let mut pvt = PVT {
       position: None,
       velocity: None,
       time: None,
     };
 
-    self.read_packets(|packet| match packet {
-      PacketRef::NavPvt(sol) => {
-        let has_time = sol.fix_type() == GpsFix::Fix3D
-          || sol.fix_type() == GpsFix::GPSPlusDeadReckoning
-          || sol.fix_type() == GpsFix::TimeOnlyFix;
-        let has_posvel = sol.fix_type() == GpsFix::Fix3D
-          || sol.fix_type() == GpsFix::GPSPlusDeadReckoning;
+    self
+      .read_packets(|packet| match packet {
+        PacketRef::NavPvt(sol) => {
+          let has_time = sol.fix_type() == GpsFix::Fix3D
+            || sol.fix_type() == GpsFix::GPSPlusDeadReckoning
+            || sol.fix_type() == GpsFix::TimeOnlyFix;
+          let has_posvel = sol.fix_type() == GpsFix::Fix3D
+            || sol.fix_type() == GpsFix::GPSPlusDeadReckoning;
 
-        if has_posvel {
-          let pos: Position = (&sol).into();
-          let vel: Velocity = (&sol).into();
+          if has_posvel {
+            let pos: Position = (&sol).into();
+            let vel: Velocity = (&sol).into();
 
-          println!(
-            "Latitude: {:.5} Longitude: {:.5} Altitude: {:.2}m",
-            pos.lat, pos.lon, pos.alt
-          );
-          println!(
-            "Speed: {:.2} m/s Heading: {:.2} degrees",
-            vel.speed, vel.heading
-          );
-          println!("Sol: {:?}", sol);
+            println!(
+              "Latitude: {:.5} Longitude: {:.5} Altitude: {:.2}m",
+              pos.lat, pos.lon, pos.alt
+            );
+            println!(
+              "Speed: {:.2} m/s Heading: {:.2} degrees",
+              vel.speed, vel.heading
+            );
+            println!("Sol: {:?}", sol);
 
-          pvt.position = Some(pos);
-          pvt.velocity = Some(vel);
-        }
+            pvt.position = Some(pos);
+            pvt.velocity = Some(vel);
+          }
 
-        if has_time {
-          if let Ok(time) = (&sol).try_into() {
-            let time: DateTime<Utc> = time;
-            println!("Time: {:?}", time);
-            pvt.time = Some(time);
-          } else {
-            println!("Could not parse NAV-PVT time field to UTC");
+          if has_time {
+            if let Ok(time) = (&sol).try_into() {
+              let time: DateTime<Utc> = time;
+              println!("Time: {:?}", time);
+              pvt.time = Some(time);
+            } else {
+              println!("Could not parse NAV-PVT time field to UTC");
+            }
           }
         }
-      }
-      _ => {
-        println!("{:?}", packet); // some other packet
-      }
-    });
+        _ => {
+          println!("{:?}", packet); // some other packet
+        }
+      })
+      .map_err(GPSError::GPSMessage)?;
 
     if pvt.position.is_some() || pvt.velocity.is_some() || pvt.time.is_some() {
-      Some(pvt)
+      Ok(Some(pvt))
     } else {
-      None
+      Err(GPSError::GPSMessage(std::io::Error::new(
+        std::io::ErrorKind::NotFound,
+        "No valid NAV-PVT response received from the device.",
+      )))
     }
   }
 
@@ -223,7 +270,7 @@ impl GPS {
       const MAX_PAYLOAD_LEN: usize = 1240;
       let mut local_buf = [0; MAX_PAYLOAD_LEN];
       self.select_chip()?;
-      let nbytes = self.spidev.read(&mut buffer)?;
+      let nbytes = self.spidev.read(&mut local_buf)?;
       self.deselect_chip()?;
       if nbytes == 0 {
         break;
@@ -267,15 +314,15 @@ impl GPS {
   // Calculates the UBX checksum for a given payload
   // Useful if we need to manually construct packets
   // See Interface Description Section 3.4
-  fn calculate_checksum(&self, payload: &[u8]) -> (u8, u8) {
-    let mut ck_a = 0u8;
-    let mut ck_b = 0u8;
-    for byte in payload {
-      ck_a = ck_a.wrapping_add(*byte);
-      ck_b = ck_b.wrapping_add(ck_a);
-    }
-    (ck_a, ck_b)
-  }
+  // fn calculate_checksum(&self, payload: &[u8]) -> (u8, u8) {
+  //   let mut ck_a = 0u8;
+  //   let mut ck_b = 0u8;
+  //   for byte in payload {
+  //     ck_a = ck_a.wrapping_add(*byte);
+  //     ck_b = ck_b.wrapping_add(ck_a);
+  //   }
+  //   (ck_a, ck_b)
+  // }
 
   // Asserts the chip select line (active low).
   fn select_chip(&mut self) -> std::io::Result<()> {
